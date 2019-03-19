443ff38727d9bd7f47deddac259b5e8d8a11f93f
juheesvt <38813640+juheesvt@users.noreply.github.com>
2019-03-19 10:52:47 +0900

    숫자 지그재그로 출력하기 : 점화식 이용

--- 2019-cpp-project/output-zigzag.cpp
+++ 2019-cpp-project/output-zigzag.cpp
@@ -6,17 +6,48 @@ void outputZigZag(int n, int k);
 
 int main() {
 
+	int t;
+	int n, k;
+
+	cin >> t;
+
+	for (int i = 0; i < t; ++i) {
+		cin >> n >> k;
+		outputZigZag(n, k);
+	}
+
 	return 0;
 }
 
 void outputZigZag(int n, int k) {
 
-	for (int row = 1; row <= n; ++row) {
+	if (k % 2 == 0) {
 
-		if (row % 2 ) {
+		int even = k * (k / 2) + (k / 2);
+		
+		for (int i = even ; i > even - k; --i) {
 
+			if (i == even - k+1)
+				cout << i;
+			else
+				cout << i << "*";
+		}
+		cout << endl;
+	}
+
+	else {
+
+		int odd = k * (k / 2) + 1;
+
+		for (int i = odd; i < odd + k; ++i) {
+
+			if (i == odd + k-1)
+				cout << i;
+			else
+				cout << i << "*";
 
 		}
+		cout << endl;
 	}
 
 }
